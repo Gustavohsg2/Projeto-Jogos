cd12274ea1ecd0a3bb1c8d39c24dc3116dd725dd
Gustavohsg2 <102223272+Gustavohsg2@users.noreply.github.com>
2023-03-23 11:54:08 -0300

    Scripts em C# de um projeto de jogos

---
+++ bomba.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomba : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D objetoTocado){
+		string tagTocada = objetoTocado.gameObject.tag; 
+
+		if (tagTocada == "acertar1") {
+			Destroy (objetoTocado.gameObject);
+			Destroy (gameObject);
+		}	
+	}
+}
+
